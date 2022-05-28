7590c7ed74ab4fe7147646a09467e72a3082d3c9
Niels-Bored <Niels-Bore@users.noreply.github.com>
2022-05-28 13:20:06 -0500

    Busquedas con extra

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonsService } from 'src/app/services/pokemons.service';
@@ -10,13 +11,26 @@ import { PokemonsService } from 'src/app/services/pokemons.service';
 export class PokemonComponent implements OnInit {
   nombre:string;
   pokemon:any;
+  tipos : any[] = [];
+  habilidades : any[] = [];
+  juegos : any[] = [];
+  mostrar : boolean;
   constructor(public activatedRoute:ActivatedRoute, private consulta:PokemonsService) { 
     this.activatedRoute.params.subscribe(params=>{
+      this.nombre = params['nombre'];
       this.consulta.getPokemon(params['nombre']).subscribe((res: any) => {
-        this.nombre = params['nombre'];
         this.pokemon = res;
+        this.tipos = this.pokemon.types;
+        this.habilidades = this.pokemon.abilities;
+        this.juegos = this.pokemon.game_indices;
         console.log(this.pokemon);
         console.log(this.pokemon.sprites.front_default)
+        console.log(this.habilidades);
+        console.log(this.juegos);
+        this.mostrar = true;
+      }, (error:any) =>{
+        console.log("Ha habido un problema");
+        this.mostrar = false;
       });  
     })
   }
